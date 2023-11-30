bfa138f7aca458c484aa9f9c4d2badd75f6533bf
dwiprst13 <dwiprst13@gmail.com>
2023-11-30 14:30:43 +0700

    new

--- artikel.php
+++ artikel.php
@@ -15,10 +15,10 @@ $queryArtikel = mysqli_query($conn, "SELECT * FROM artikel WHERE status = 'publi
                 $path_baru = str_replace('../../', '', $path_relatif);
                 ?>
                     <a href="#" class="flex items-center bg-white border border-gray-200 rounded-lg col-span-6">
-                        <img class="object-fill w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="<?= $path_baru ?>" style="width: 200px; height: 150px;" alt="">
+                        <img class="object-fill flex-wrap w-full rounded-t-lg h-48 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="<?= $path_baru ?>" style="width: 200px; height: 150px;" alt="">
                         <div class="flex flex-col justify-between p-4 leading-normal">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "><?= $row_artikel['judul'] ?></h5>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-3"><?= $row_artikel['content'] ?></p>
+                            <h5 class="mb-2 flex-wrap text-2xl font-bold tracking-tight text-gray-900 "><?= $row_artikel['judul'] ?></h5>
+                            <p class="mb-3 flex-wrap font-normal text-gray-700 dark:text-gray-400 line-clamp-3"><?= $row_artikel['content'] ?></p>
                         </div>
                     </a>
                 <?php
